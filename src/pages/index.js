8dba9104f193afada8118a3daeb183b0b7b5114b
Raul Nicolas <raul.nicolas@netcentric.biz>
2020-03-27 16:20:44 +0100

    css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,10 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
-    <Link to="/about/">About</Link>
+    <h2><Link to="/about/">About</Link></h2>
+    <h2><Link to="/contact/">Contact</Link></h2>
   </div>
 )
